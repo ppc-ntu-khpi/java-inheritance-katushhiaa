9392a777cf58539e5f0571eaf2355c84a4739c84
katushhiaa <113555695+katushhiaa@users.noreply.github.com>
2023-02-17 14:11:21 +0200

    Add files via upload

---
+++ src/domain/domain/Predator.java
@@ -0,0 +1,11 @@
+package domain ;
+
+public class Predator extends Animal
+{
+
+	public void hunt() {
+            System.out.println("Predator is hunting, but our kangaroo isn't a predator");
+	}
+	
+}
+
