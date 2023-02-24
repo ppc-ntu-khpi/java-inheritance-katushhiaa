d7f69563b55aa813858fc6e9cc655114aa1c7cd4
katushhiaa <113555695+katushhiaa@users.noreply.github.com>
2023-02-24 15:46:03 +0200

    Add files via upload

---
+++ docs/search/all_a.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['sleep_0',['sleep',['../classdomain_1_1_animal.html#ad168ff1deb4cf3e0628fa8ed6bdacc30',1,'domain.Animal.sleep()'],['../classdomain_1_1_kangaroo.html#a592ba4769146af527f5c90faafdcfaf5',1,'domain.Kangaroo.sleep()']]],
+  ['speak_1',['speak',['../classdomain_1_1_animal.html#a0070dca2022620de6da5c005e04609a6',1,'domain.Animal.speak()'],['../classdomain_1_1_kangaroo.html#a37810a29ae830a5dde6177c1b139dce5',1,'domain.Kangaroo.speak()']]],
+  ['swim_2',['swim',['../classdomain_1_1_kangaroo.html#abb82ba44edfbbcf9cd4993cc56235819',1,'domain::Kangaroo']]]
+];
